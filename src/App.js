6594ca3e941ff1cf57ad8ba4fb88c22bb5d93600
Melanie Silies <melanie.silies@web.de>
2023-06-12 14:08:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'//Main React.js library
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const data = {
-	image: "https://ucarecdn.com/f8cf81eb-3bab-4bba-9431-668884eab174/-/resize/300x/",
+	image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Bob_Dylan_-_Azkena_Rock_Festival_2010_2.jpg/800px-Bob_Dylan_-_Azkena_Rock_Festival_2010_2.jpg",
 	cardTitle: "Bob Dylan",
 	cardDescription:
 		"Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter, author, and artist who has been an influential figure in popular music and culture for more than five decades.",
@@ -14,9 +16,15 @@ const data = {
 const App = () => {
   return (
     <div>
-        // your code here
-
-    </div>
+    <div className="card m-5">
+  	<img className="card-img-top" src= {data.image} alt="Bob Dylan" />
+  	<div className="card-body">
+    <h5 className="card-title">{data.cardTitle}</h5>
+    <p className="card-text">{data.cardDescription}</p>
+    <a href={data.button.url} className="btn btn-primary">{data.button.label}</a>
+  </div>
+</div>
+</div>
   )
 }
 
